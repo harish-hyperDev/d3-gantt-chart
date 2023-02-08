21d77926b743b69ebc49247e6137d5e047eba569
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-02-08 19:05:26 +0530

    got dates

--- src/common.js
+++ src/common.js
@@ -1,19 +1,22 @@
 
 var tasks = [
-  { "startDate": new Date("Sun Dec 09 00:00:45 EST 2012"), "endDate": new Date("Sun Dec 09 02:36:45 EST 2012"), "taskName": "Interested", "status": "RUNNING" },
-  { "startDate": new Date("Sun Dec 09 08:49:53 EST 2012"), "endDate": new Date("Sun Dec 09 06:34:04 EST 2012"), "taskName": "Interested", "status": "RUNNING" },
-  { "startDate": new Date("Sun Dec 09 03:27:35 EST 2012"), "endDate": new Date("Sun Dec 09 03:58:43 EST 2012"), "taskName": "Interested", "status": "SUCCEEDED" },
-  { "startDate": new Date("Sun Dec 09 03:27:35 EST 2012"), "endDate": new Date("Sun Dec 09 03:58:43 EST 2012"), "taskName": "Interested", "status": "KILLED" }
+  { "startDate": new Date("Sun Dec 09 00:00:45 EST 2012"), "endDate": new Date("Sun Dec 09 02:36:45 EST 2012"), "taskName": "02/03/2023", "status": "RUNNING" },
+  { "startDate": new Date("Sun Dec 03 03:27:35 EST 2012"), "endDate": new Date("Sun Dec 03 04:58:43 EST 2012"), "taskName": "02/03/2023", "status": "RUNNING" },
+  { "startDate": new Date("Sun Dec 09 03:27:35 EST 2012"), "endDate": new Date("Sun Dec 09 03:58:43 EST 2012"), "taskName": "02/03/2023", "status": "SUCCEEDED" },
+  { "startDate": new Date("Sun Dec 09 03:27:35 EST 2012"), "endDate": new Date("Sun Dec 09 03:58:43 EST 2012"), "taskName": "04/03/2023", "status": "KILLED" },
+  { "startDate": new Date("Sun Dec 09 03:27:35 EST 2012"), "endDate": new Date("Sun Dec 09 04:58:43 EST 2012"), "taskName": "06/03/2023", "status": "SUCCEEDED" }
 ];
 
 // console.log(new Date(25/01/2017))
 // console.log(d3.days.format("%d/%m/%y"))
 
 
-const formatTime = d3.timeFormat("%d %b %Y")
-const parseTime = d3.timeParse("%d %b %Y")
+const formatTime = d3.timeFormat("%d/%b/%Y")
+const parseTime = d3.timeParse("%d/%b/%Y")
 
-console.log("time format ", parseTime(formatTime(new Date("07 May 2022 "))))
+var mydate = moment('07/03/2022', 'DD/MM/YYYY');
+
+console.log("time format ", parseTime(formatTime(new Date(moment(mydate).format("MM/DD/YYYY")))))
 // var svg2 = d3.select("body").append("svg")
 // .attr("width", 960)
 // .attr("height", 100)
@@ -29,14 +32,49 @@ var taskStatus = {
   "KILLED": "bar-killed"
 };
 
+// RegEx for duration HH:MM AM/PM --> /((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))/
 
-d3.csv('../data/timeline_sample.csv', function (data) {
+var d;
+var drawChatFromData = d3.csv('../data/timeline_sample.csv', function (data) {
   // console.log(this)
-
   data = data.filter(d => d['level_0'] !== '')
-  
+  console.log(data)
 
-  var taskNames = ["Interested", "Not Interested"];
+  var callsData;
+  var dates;
+
+  /* dump = data.map(((d,i) => {
+    let filterDataByDates = []
+    console.log(Object.keys(d))
+
+    const dateRegEx = /(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}/
+    if(dateRegEx.test(Object.keys(d).map(d => {return d}))) {
+      console.log(Object.keys(d))
+    }
+    // console.log(d['02/02/2023'])
+  })) */
+
+  // dump = data.map(d)
+
+  const dateRegEx = /(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}/
+  let filterCallsByDate = []
+
+  /* for (i in data) {
+    console.log(data[i])
+    for (j,k in Object.keys(data[i])) {
+      console.log(Object.keys(data[i]))
+      
+    } 
+    
+  } */
+
+  dates = Object.keys(data[0]).filter((d) => dateRegEx.test(d) && data.filter(dk => d[dk] !== '') )
+  console.log("dates ", dates)
+
+  filterCallsByDate = data.map(d => dates.map(date => d[date] ))
+  console.log(filterCallsByDate)
+
+  var taskNames = ["01/03/2023", "02/03/2023", "03/03/2023", "04/03/2023", "05/03/2023", "06/03/2023", "07/03/2023"];
 
   tasks.sort(function (a, b) {
     return a.endDate - b.endDate;
@@ -50,7 +88,7 @@ d3.csv('../data/timeline_sample.csv', function (data) {
   var format = "%H:%M";
   var timeDomainString = "1day";
 
-  var gantt = d3.gantt().height(450).width(800).taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);
+  var gantt = d3.gantt().height(800).width(800).taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);
 
   // gantt.timeDomainMode("fixed");
   changeTimeDomain(timeDomainString);
@@ -99,7 +137,7 @@ d3.csv('../data/timeline_sample.csv', function (data) {
   }
 
   function addTask() {
-
+    console.log("called")
     var lastEndDate = getEndDate();
     var taskStatusKeys = Object.keys(taskStatus);
     var taskStatusName = taskStatusKeys[Math.floor(Math.random() * taskStatusKeys.length)];
@@ -122,4 +160,49 @@ d3.csv('../data/timeline_sample.csv', function (data) {
     gantt.redraw(tasks);
   };
 
+  document.getElementsByClassName("add")[0].addEventListener('click', addTask )
+  document.getElementsByClassName("delete")[0].addEventListener('click', removeTask)
+  document.getElementsByClassName("1hr")[0].addEventListener('click', () => changeTimeDomain("1hr"))
+  document.getElementsByClassName("3hr")[0].addEventListener('click', () => changeTimeDomain("3hr"))
+  document.getElementsByClassName("6hr")[0].addEventListener('click', () => changeTimeDomain("6hr"))
+  document.getElementsByClassName("1d")[0].addEventListener('click', () => changeTimeDomain("1day"))
+  document.getElementsByClassName("1w")[0].addEventListener('click', () => changeTimeDomain("1week"))
 })
+
+
+/* var f = drawChatFromData.get("../data/timeline_sample.csv", (a) => {return (a)})
+console.log(f)  */
+
+// console.log("d",d)
+
+
+/* const downloadCsv = async () => {
+  try {
+      const target = `../data/timeline_sample.csv`; //file
+      //const target = `https://SOME_DOMAIN.com/api/data/log_csv?$"queryString"`; //target can also be api with req.query
+      
+      const res = await fetch(target, {
+          method: 'get',
+          headers: {
+              'content-type': 'text/csv;charset=UTF-8',
+              //'Authorization': //in case you need authorisation
+          }
+      });
+
+      if (res.status === 200) {
+
+          const data = await res.text();
+          // console.log(data);
+          return data
+
+      } else {
+          console.log(`Error code ${res.status}`);
+      }
+  } catch (err) {
+      console.log(err)
+  }
+}
+console.log(e)
+e = downloadCsv();
+console.log(e) */
+
